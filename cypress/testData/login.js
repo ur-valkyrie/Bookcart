b75d4cc82e13e109679b6ccc7271d5dc58b62f47
ur-valkyrie <castiel.andreeva@gmail.com>
2023-12-18 17:27:58 +0100

    uploaded files

--- cypress/testData/login.js
+++ cypress/testData/login.js
@@ -3,24 +3,47 @@ import { signUp } from "./signUp";
 
 let userID;
 let token;
-export function login() {
-    return signUp().then(({ username, password }) => {
-        cy.request({
-            method: "POST",
-            url: "https://bookcart.azurewebsites.net/api/login",
-            body: {
-                username: username,
-                password: password,
-            },
-        }).then((response) => {
-            userID = response.body.userDetails.userId;
-            token = response.body.token;
-            cy.log(response);
-            cy.log(response.body.token);
-            cy.log(response.body.userDetails.userId);
-            cy.log(response.status);
-        });
-    });
-}
+// export function login() {
+//     return signUp().then(({ username, password }) => {
+//         cy.request({
+//             method: "POST",
+//             url: "https://bookcart.azurewebsites.net/api/login",
+//             body: {
+//                 username: username,
+//                 password: password,
+//             },
+//         }).then((response) => {
+//             userID = response.body.userDetails.userId;
+//             token = response.body.token;
+//             cy.log(response);
+//             cy.log(response.body.token);
+//             cy.log(response.body.userDetails.userId);
+//             cy.log(response.status);
+//         });
+//     });
+// }
 
-export { userID }
+// export { userID }
+
+
+export function login() {
+    signUp().then(({ username, password }) => {
+      cy.request({
+        method: "POST",
+        url: "https://bookcart.azurewebsites.net/api/login",
+        body: {
+          username: username,
+          password: password,
+        },
+      }).then((response) => {
+        userID = response.body.userDetails.userId;
+        token = response.body.token;
+        cy.log(response);
+        cy.log(response.body.token);
+        cy.log(response.body.userDetails.userId);
+        cy.log(response.status);
+    });
+    });
+  }
+
+  export { userID }
--- cypress/testData/signUp.js
+++ cypress/testData/signUp.js
@@ -1,25 +1,45 @@
 /// <reference types="Cypress" />
 import { faker } from '@faker-js/faker';
 
-export function signUp() {
-    return new Promise((resolve) => {
-        const username = faker.internet.userName();
-        const password = faker.internet.password();
+// export function signUp() {
+//     return new Promise((resolve) => {
+//         const username = faker.internet.userName();
+//         const password = faker.internet.password();
 
-        cy.request({
-            method: "POST",
-            url: "https://bookcart.azurewebsites.net/api/User",
-            body: {
-                firstName: faker.person.firstName(),
-                lastName: faker.person.lastName(),
-                username: username,
-                password: password,
-                confirmPassword: faker.internet.password(),
-                gender: faker.person.sexType(),
-            },
-        }).then((response) => {
-            cy.log(response);
-            resolve({ username, password });
-        });
+//         cy.request({
+//             method: "POST",
+//             url: "https://bookcart.azurewebsites.net/api/User",
+//             body: {
+//                 firstName: faker.person.firstName(),
+//                 lastName: faker.person.lastName(),
+//                 username: username,
+//                 password: password,
+//                 confirmPassword: faker.internet.password(),
+//                 gender: faker.person.sexType(),
+//             },
+//         }).then((response) => {
+//             cy.log(response);
+//             resolve({ username, password });
+//         });
+//     });
+// }
+
+export function signUp() {
+    const username = faker.internet.userName();
+    const password = faker.internet.password();
+  
+    return cy.request({
+        method: "POST",
+        url: "https://bookcart.azurewebsites.net/api/User",
+        body: {
+            firstName: faker.person.firstName(),
+            lastName: faker.person.lastName(),
+            username: username,
+            password: password,
+            confirmPassword: faker.internet.password(),
+            gender: faker.person.sexType(),
+        },
+    }).then(() => {
+        return { username, password };
     });
-}
+  }
