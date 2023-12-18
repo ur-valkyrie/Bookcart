93792b46880d3073d9cf46470a885e3fd912dabd
ur-valkyrie <castiel.andreeva@gmail.com>
2023-12-18 17:50:34 +0100

    uploaded files

--- cypress/testData/verificationOfUser.js
+++ cypress/testData/verificationOfUser.js
@@ -2,17 +2,6 @@
 import { login } from "./login";
 import { userID } from "./login";
 
-// export function getUser() {
-//   login().then(() => {
-//     cy.request({
-//       method: "GET",
-//       url: `https://bookcart.azurewebsites.net/api/User/${userID}`,
-//     }).then((response) => {
-//       expect(response.status).to.equal(200);
-//     });
-//   });
-// }
-
 export function getUser() {
   login().then(() => {
       cy.request({
